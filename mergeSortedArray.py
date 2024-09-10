d88ab09a974318d2d1775525c5e073c364b65726
Areej Aasna <73154198+areejaasna@users.noreply.github.com>
2024-09-10 18:04:38 +0530

    mergeSortedArray.py

--- mergeSortedArray.py
+++ mergeSortedArray.py
@@ -1,4 +1,4 @@
-#merge sorted array without using extra space
+#merge two sorted array without using extra space
 def mergeSorted(arr1, arr2):
     n = len(arr1)
     m = len(arr2)
@@ -7,8 +7,8 @@ def mergeSorted(arr1, arr2):
     while (left >=0 and right < m):
         if(arr1[left] > arr2[right]):
             arr1[left], arr2[right] = arr2[right], arr1[left]
-            left -=1
-            right +=1
+        left -=1
+        right +=1
         else:
             break
     arr1.sort()
